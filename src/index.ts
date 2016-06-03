00a7c7615bd7bd0513b319064aabf8b04a3e63ae
Arnaud Bosc <arnaud.bosc@elium.tv>
2016-06-03 16:04:41 +0200

    fix: main barrel

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from "./layers";
-
+export * from "./http.adapter";
+export * from "./http.formatter";
+export * from "./http.parser";
+export * from "./http.request";
+export * from "./http.response";
