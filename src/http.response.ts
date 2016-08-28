22d677cb127c40aaedc80af8f77bed8421c6d2fb
Arnaud Bosc <bosc.arno@gmail.com>
2016-08-28 08:49:41 +0200

    fix: status code

--- src/http.response.ts
+++ src/http.response.ts
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import {IResponse, Response} from '@elium/mighty-js';
 
 export interface IHttpResponse extends IResponse {
@@ -5,5 +6,10 @@ export interface IHttpResponse extends IResponse {
 }
 
 export class HttpResponse extends Response implements IHttpResponse {
-  status: number = 0;
+  status: number;
+  
+  constructor(config?: IHttpResponse) {
+    super(config);
+    this.status = _.get(config, "status", 0);
+  }
 }
