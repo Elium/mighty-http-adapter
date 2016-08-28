98bc8b83679c9ba7fce185ef1e137ac29f418579
Arnaud Bosc <bosc.arno@gmail.com>
2016-08-28 08:10:42 +0200

    feat: add http response status code

--- src/http.response.ts
+++ src/http.response.ts
@@ -1,4 +1,9 @@
 import {IResponse, Response} from '@elium/mighty-js';
 
-export interface IHttpResponse extends IResponse {}
-export class HttpResponse extends Response implements IHttpResponse {}
+export interface IHttpResponse extends IResponse {
+  status: number
+}
+
+export class HttpResponse extends Response implements IHttpResponse {
+  status: number = 0;
+}
