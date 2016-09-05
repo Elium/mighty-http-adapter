6a72c7346edcbcafc6c0e66716e7aafe56b169e3
Arnaud Bosc <bosc.arno@gmail.com>
2016-09-05 20:15:52 +0200

    feat: allow baseUrl and layer to be publicly overriden

--- src/http.adapter.ts
+++ src/http.adapter.ts
@@ -1,38 +1,43 @@
-import {Adapter, IResource, IResponse, IRecord} from '@elium/mighty-js';
+import {Adapter, IResource, IResponse, IRecord, IAdapter} from '@elium/mighty-js';
 import {IHttpRequest, HttpRequest} from './http.request';
 import {IDataLayer} from './layer';
 
-export class HttpAdapter extends Adapter {
-  protected _dataLayer: IDataLayer;
-  protected _baseUrl: string;
+export interface IHttpAdapter extends IAdapter {
+  dataLayer: IDataLayer;
+  baseUrl: string;
+}
+
+export class HttpAdapter extends Adapter implements IHttpAdapter {
+  dataLayer: IDataLayer;
+  baseUrl: string;
   
   constructor(baseURl?: string, dataLayer?: IDataLayer) {
     super();
-    this._baseUrl = baseURl || "";
-    this._dataLayer = dataLayer;
+    this.baseUrl = baseURl || "";
+    this.dataLayer = dataLayer;
   }
   
   create<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.create(this._getRequest(resource, request));
+    return this.dataLayer.create(this._getRequest(resource, request));
   }
   
   findOne<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.findOne(this._getRequest(resource, request));
+    return this.dataLayer.findOne(this._getRequest(resource, request));
   }
   
   find<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.find(this._getRequest(resource, request).merge(<IHttpRequest> {isArray: true}));
+    return this.dataLayer.find(this._getRequest(resource, request).merge(<IHttpRequest> {isArray: true}));
   }
   
   save<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.save(this._getRequest(resource, request));
+    return this.dataLayer.save(this._getRequest(resource, request));
   }
   
   destroy<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.destroy(this._getRequest(resource, request));
+    return this.dataLayer.destroy(this._getRequest(resource, request));
   }
   
   protected _getRequest<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): HttpRequest {
-    return new HttpRequest({url: `${this._baseUrl}/${resource.identity}`}).merge(request);
+    return new HttpRequest({url: `${this.baseUrl}/${resource.identity}`}).merge(request);
   }
 }
--- src/rest.adapter.ts
+++ src/rest.adapter.ts
@@ -5,18 +5,18 @@ import {IHttpRequest, HttpRequest} from './http.request';
 export class RestAdapter extends HttpAdapter {
   
   findOne<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.findOne(this._getRequestWithId(resource, request));
+    return this.dataLayer.findOne(this._getRequestWithId(resource, request));
   }
   
   save<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.save(this._getRequestWithId(resource, request));
+    return this.dataLayer.save(this._getRequestWithId(resource, request));
   }
   
   destroy<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest): Promise<IResponse> {
-    return this._dataLayer.destroy(this._getRequestWithId(resource, request));
+    return this.dataLayer.destroy(this._getRequestWithId(resource, request));
   }
   
   protected _getRequestWithId<R extends IRecord>(resource: IResource<R, this>, request: IHttpRequest) {
-    return new HttpRequest({url: `${this._baseUrl}/${resource.identity}/${request.criteria["id"] || request.data.id}`}).merge(request);
+    return new HttpRequest({url: `${this.baseUrl}/${resource.identity}/${request.criteria["id"] || request.data.id}`}).merge(request);
   }
 }
