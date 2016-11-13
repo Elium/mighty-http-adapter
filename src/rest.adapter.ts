9343e3ca7c70985b611f0b2cdb8df87ad4b855cb
Arnaud Bosc <bosc.arno@gmail.com>
2016-11-13 17:13:54 +0100

    4.0.4

--- src/rest.adapter.ts
+++ src/rest.adapter.ts
@@ -18,6 +18,7 @@ export class RestAdapter extends HttpAdapter {
   }
 
   protected _getRequestWithId<R extends IRecord>(resource: IResource<R>, request: IHttpRequest) {
-    return _.merge({}, request, {url: request.url || `${this.baseUrl}/${resource.identity}/${request.criteria["id"] || request.data.id}`});
+    const id = _.get(request, 'criteria.id', null) || _.get(request, 'data.id', "");
+    return _.merge({}, request, {url: request.url || `${this.baseUrl}/${resource.identity}/${id}`});
   }
 }
