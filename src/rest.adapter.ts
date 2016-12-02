e011169ce197b71e66c4965f4c3c8505cb2916ac
Benjamin Longearet <firehist@gmail.com>
2016-12-02 07:04:41 +0100

    Add a beforeRequest hook

--- src/rest.adapter.ts
+++ src/rest.adapter.ts
@@ -6,25 +6,31 @@ import {IHttpRequest, HttpRequest} from './http.request';
 export class RestAdapter extends HttpAdapter {
 
   findOne<R extends IRecord>(resource: IResource<R>, request: IHttpRequest): Promise<IResponse> {
-    return this.applyHook('beforeFindOne', this._getRequestWithId(resource, request))
+    return this._getRequestWithId(resource, request)
+      .then(baseRequest => this.applyHook('beforeFindOne', baseRequest))
       .then(newRequest => this.dataLayer.findOne(newRequest))
       .then(response => this.applyHook('afterFindOne', response));
   }
 
   save<R extends IRecord>(resource: IResource<R>, request: IHttpRequest): Promise<IResponse> {
-    return this.applyHook('beforeSave', this._getRequestWithId(resource, request))
+    return this._getRequestWithId(resource, request)
+      .then(baseRequest => this.applyHook('beforeSave', baseRequest))
       .then(newRequest => this.dataLayer.save(newRequest))
       .then(response => this.applyHook('afterSave', response));
   }
 
   destroy<R extends IRecord>(resource: IResource<R>, request: IHttpRequest): Promise<IResponse> {
-    return this.applyHook('beforeDestroy', this._getRequestWithId(resource, request))
+    return this._getRequestWithId(resource, request)
+      .then(baseRequest => this.applyHook('beforeDestroy', baseRequest))
       .then(newRequest => this.dataLayer.destroy(newRequest))
       .then(response => this.applyHook('afterDestroy', response));
   }
 
   protected _getRequestWithId<R extends IRecord>(resource: IResource<R>, request: IHttpRequest) {
-    const id = _.get(request, 'criteria.id', null) || _.get(request, 'data.id', "");
-    return _.merge({}, request, {url: request.url || `${this.baseUrl}/${resource.identity}/${id}`});
+    return this.applyHook('beforeRequest', request)
+      .then(newRequest => {
+        const id = _.get(newRequest, 'criteria.id', null) || _.get(newRequest, 'data.id', "");    
+        return _.merge({}, request, {url: request.url || `${this.baseUrl}/${resource.identity}/${id}`});
+      });
   }
 }
